65e6318a76b88b1134508c84efaf3b95fbd0c7b7
prephyte <prephyte@protonmail.com>
2022-01-14 05:50:49 +0300

    add password and worker name to nicehash

--- .scripts/nicehash/start.sh
+++ .scripts/nicehash/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 /opt/t-rex/t-rex -a ethash -o stratum+tcp://daggerhashimoto.eu-west.nicehash.com:3353 \
-    -u 3M53dNMuzJrocFF9DDi5FCqNu67iQsL2F2 \
+    -u 3M53dNMuzJrocFF9DDi5FCqNu67iQsL2F2.PC \
     -p x \
     -i 15 \
+    -w PC \
     --api-bind-http "0.0.0.0:4067" \
     --temperature-limit "59" \
     --temperature-start "55" \
+    --api-key "$(cat ~/.secrets/t-rex.secret)" \
     > /dev/null
